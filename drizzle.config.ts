9e863d5508d1080fc615b502e9c624445eb007d9
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2026-04-08 21:33:12 +0400

    drizzle config fixes

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,8 @@
-import { defineConfig } from 'drizzle-kit';
+// import "dotenv/config";
+import { config } from "dotenv";
+import { defineConfig } from "drizzle-kit";
+
+ config({ path: ".env.local" });
 
 if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
